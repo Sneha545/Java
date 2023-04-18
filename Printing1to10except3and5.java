35868aac95c85dfc3145c166430a808905726ddc
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-19 00:14:09 +0530

    Create Printing1to10except3and5.java

---
+++ Printing1to10except3and5.java
@@ -0,0 +1,14 @@
+//program to print 1 to 10 numbers except 3 and 5
+public class Printing1to10except3and5
+{
+	public static void main(String[] args) {
+	    for(int i=1;i<=10;i++)
+	    {
+	        if(i!=3 && i!=7)
+	        {
+	            System.out.println(i);
+	        }
+	    }
+		
+	}
+}
