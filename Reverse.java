7f978edd34847e4cbc112ea45855c323d872314b
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-19 00:26:54 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+//program to print 1 to 10 natural numbers in reverse order
+public class Reverse
+{
+	public static void main(String[] args) {
+	    for(int i=10;i>=1;i--)
+	    {
+	      System.out.println(i);  
+	    }
+		
+	}
+}
