58a97aa06230c3c8d7d8d46d227ebe59a8d520d8
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-19 00:28:22 +0530

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,16 @@
+//program to print user name user defined times
+import java.util.Scanner;
+public class Username
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter the input");
+	    int n = scan.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        System.out.println("Sneha");
+	        
+	    }
+		
+	}
+}
