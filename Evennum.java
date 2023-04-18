c16b2bd03253dc030574c3f79535e8a1a30178ca
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-19 00:24:41 +0530

    Create Evennum.java

---
+++ Evennum.java
@@ -0,0 +1,21 @@
+//program to print even numbers within a range
+import java.util.Scanner;
+public class Evennum
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter the min range");
+	    int n = scan.nextInt();
+	    System.out.println("Enbter the max range");
+	    int s = scan.nextInt();
+	    for(int i=n;i<=s;i++)
+	    {
+	        if(i%2 == 0)
+	        {
+	            System.out.println(i);
+	
+	        }
+	    }
+
+	}
+}
