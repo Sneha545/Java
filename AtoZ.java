3dc1fc189fb14b0213dfb2404767ec0c5cad6dec
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-19 00:29:21 +0530

    Create  AtoZ.java

---
+++ AtoZ.java
@@ -0,0 +1,11 @@
+//program to print alphabets from A to Z
+public class AtoZ
+{
+	public static void main(String[] args) 
+	{
+	 for(char ch='A';ch<='Z';ch++)
+	  {
+			System.out.println(ch);
+	  }
+}
+}
