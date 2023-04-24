948e73748dc789072e3cc458a4588794c8e4251f
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-24 21:46:26 +0530

    Create firstnspy.java

---
+++ firstnspy.java
@@ -0,0 +1,31 @@
+//program to print first n spy Number
+import java.util.Scanner;
+public class firstnspy
+{
+	public static void main(String[] args) 
+	{
+	    Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = scan.nextInt();
+		int sln=1;
+		for(int i=1;sln<=n;i++)
+		{
+		    int temp=i;
+		    int sum=0;
+		    int prod=1;
+		    while(temp!=0)
+		    {
+		        int ld = temp%10;
+		        sum=sum+ld;
+		        prod=prod*ld;
+		        temp=temp/10;
+		    }
+		    if(sum==prod)
+		    {
+		       System.out.println(sln+")"+i);
+		       sln++; 
+		    }
+		    
+		}
+	}
+}
