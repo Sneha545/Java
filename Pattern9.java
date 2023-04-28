464ba8ee0f2260174aba97cefb757334b51bd6e0
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-28 19:01:51 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,22 @@
+/*Program to print Pattern
+54321
+54321
+54321
+54321
+54321
+*/
+
+public class Pattern9
+{
+	public static void main(String[] args) 
+	{
+	    for(int i=5;i>=1;i--)
+	    {
+	        for(int j=5;j>=1;j--)
+	        {
+	            System.out.print(" "+j+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
