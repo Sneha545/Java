18f07652c3f0c1df92a3152578b678fe4a95107e
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-28 19:00:43 +0530

    Create Pattern7.java

---
+++ Pattern7.java
@@ -0,0 +1,21 @@
+/*Program to print pattern
+11111
+22222
+33333
+44444
+55555
+*/
+public class Pattern7
+{
+	public static void main(String[] args) 
+	{
+	    for(int i=1;i<=5;i++)
+	    {
+	        for(int j=1;j<=5;j++)
+	        {
+	            System.out.print(" "+i+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
