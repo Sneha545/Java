843f684cf419848a3bbb7098814c3687914db86a
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-04-28 19:02:26 +0530

    Create Pattern10.java

---
+++ Pattern10.java
@@ -0,0 +1,21 @@
+/*Program to print Pattern
+55555
+44444
+33333
+22222
+11111
+*/
+public class Pattern10
+{
+	public static void main(String[] args) 
+	{
+	    for(int i=5;i>=1;i--)
+	    {
+	        for(int j=5;j>=1;j--)
+	        {
+	            System.out.print(" "+i+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
