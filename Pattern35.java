088741e49faf84f86b3b1cf8c8aca2f3cd5f0f8c
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-05-24 20:33:43 +0530

    Pattern35.java

---
+++ Pattern35.java
@@ -0,0 +1,47 @@
+/*program to print pattern
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/
+
+public class Pattern35
+{
+	public static void main(String[] args) 
+	{
+	    int space=8;
+	    int star=1;
+	    for(int i=1;i<=9;i++)
+	    {
+	        for(int a=1;a<=star;a++)
+	        {
+	            System.out.print("*");
+	        }
+	        for(int b=1;b<=space;b++)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int c=1;c<=star;c++)
+	        {
+	            System.out.print("*");
+	        }
+	        if(i<=4)
+	        {
+	            space=space-2;
+	            star++;
+	        }
+	        else
+	        {
+	            space=space+2;
+	            star--;
+	        }
+	        System.out.println();
+	    }
+	    
+	}
+}
