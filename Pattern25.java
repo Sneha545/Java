5a24ebf4499af7e07f83e8b23d41fba751fb5a7d
Sneha <112306058+Sneha545@users.noreply.github.com>
2023-05-24 20:25:09 +0530

    Pattern25.java

---
+++ Pattern25.java
@@ -0,0 +1,28 @@
+/*program to print pattern
+  *
+   *
+    *
+     *
+      *
+     */
+public class Pattern25
+{
+	public static void main(String[] args) 
+	{
+	    for(int i=1;i<=5;i++)
+	    {
+	        for(int j=1;j<=5;j++)
+	        {
+	           if(i==j)
+	           {
+	               System.out.print("*");
+	           }
+	           else
+	           {
+	               System.out.print(" ");
+	           }
+	        }
+	        System.out.println();
+	    }
+	}
+}
